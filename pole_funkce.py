e30100c01d00fd5045511678648703c0bcfd236f
zahradnikAdam <adamstibo@seznam.cz>
2024-03-25 09:29:20 +0100

    hi
    ji

---
+++ pole_funkce.py
@@ -0,0 +1,12 @@
+cars = ["Ford", "volvo", "BMW", "audi"]
+
+for x in range(len(cars)):
+    print(f"Auto:s číslem {x-1}: {cars[x]}")
+
+prvek_minus = input("zadejte auto ktere chcete odebrat")
+
+print(" ")
+cars.append("prvek_minus ")
+
+for x in range(len(cars)):
+    print(f"Auto:s číslem {x-1}: {cars[x]}")
